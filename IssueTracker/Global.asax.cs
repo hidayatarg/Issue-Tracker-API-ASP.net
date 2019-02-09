670fcc7e6f4a13a6d0065d105157068953bfd9ae
Hidayat arghandabi <hidayatarg@gmail.com>
2019-02-09 20:50:24 +0300

    API Configuration Setup

--- IssueTracker/Global.asax.cs
+++ IssueTracker/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Routing;
 
 namespace IssueTracker
@@ -10,6 +11,8 @@ namespace IssueTracker
     {
         protected void Application_Start()
         {
+            GlobalConfiguration
+                .Configure(WebApiConfig.Register);
         }
     }
 }
